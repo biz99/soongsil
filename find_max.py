67f93dc79382d869d354bd19ef6fd277d3e70cf5
soongsil <kwsjh06@naver.com>
2024-03-12 17:40:55 +0900

    ddd

--- find_max.py
+++ find_max.py
@@ -1,17 +1,19 @@
-data = input().split()
-data_len = len(int(data))
-temp = 0
+# 숫자를 입력받아 리스트에 저장
+numbers = input("숫자들을 입력하세요 (공백으로 구분): ").split()
+
+# 입력된 문자열을 정수로 변환
+numbers = [int(num) for num in numbers]
+
+# 리스트에서 가장 큰 값을 찾아 출력
+max_number = max(numbers)
 
 def find_max_recursive():
     print("")
     
 def find_max_interative():
-    for i in range(1, data_len):
-        if data[0] < data[i]:
-            data[i] = temp
-            data[i] = data[0]
-            data[0] = temp
-    return data[9]
-        
+    numbers = [int(data) for num in data]
+    max_number = max(numbers)
+    
+    return max_number
             
-print(find_max_interative())
+print(max_number)
